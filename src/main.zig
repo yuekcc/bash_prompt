bc3d0c01e387801037111e7b01d2dd696ac5e696
yuekcc <yuekcc@qq.com>
2022-10-20 00:40:14 +0800

    feat: 支持 bash_prompt init

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,18 @@ const process = std.process;
 const styles = @import("styles.zig").styles;
 const Repo = @import("git.zig").Repo;
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer {
-        _ = gpa.deinit();
-    }
-    var allocator = gpa.allocator();
+fn shell_init(exe_name: []const u8) !void {
+    var stdout_writer = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_writer);
+    var stdout = bw.writer();
 
+    comptime var script = "PROMPT_COMMAND=\"{s}\"; export PROMPT_COMMAND; PS1=\"\\$ \"";
+    try stdout.print(script, .{exe_name});
+    try bw.flush();
+    std.process.exit(0);
+}
+
+fn print_prompt(allocator: std.mem.Allocator) !void {
     var pwd = try process.getCwdAlloc(allocator);
     defer allocator.free(pwd);
 
@@ -40,4 +45,25 @@ pub fn main() !void {
 
     try stdout.print("\n", .{});
     try bw.flush();
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        _ = gpa.deinit();
+    }
+    var allocator = gpa.allocator();
+
+    var args_iter = try std.process.argsWithAllocator(allocator);
+    defer args_iter.deinit();
+
+    _ = args_iter.skip();
+    while (args_iter.next()) |arg| {
+        if (std.mem.eql(u8, arg[0..], "init")) {
+            try shell_init("bash_prompt");
+        }
+    }
+
+    try print_prompt(allocator);
+    std.process.exit(0);
 }
