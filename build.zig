884a3e46ff4417ba992078095757f71a802ea51b
yuekcc <yuekcc@qq.com>
2023-02-08 00:05:16 +0800

    feat: 支持最近版本的 build system api

--- build.zig
+++ build.zig
@@ -9,25 +9,26 @@ const vendors = struct {
     };
 };
 
-pub fn build(b: *std.build.Builder) !void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("bash_prompt", "src/main.zig");
-    exe.addPackage(vendors.knownFolders);
+    const knownFolders = b.createModule(.{
+        .source_file = .{ .path = "vendors/known-folders/known-folders.zig" },
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "bash_prompt",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     exe.single_threaded = true;
-    exe.strip = mode != .Debug;
-    exe.want_lto = mode != .Debug;
-    exe.setBuildMode(mode);
-    exe.setTarget(target);
+    exe.strip = optimize != .Debug;
+    exe.want_lto = optimize != .Debug;
+    exe.addModule("known-folders", knownFolders);
     exe.install();
 
     const run_cmd = exe.run();
@@ -39,10 +40,8 @@ pub fn build(b: *std.build.Builder) !void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/tests.zig");
-    exe_tests.addPackage(vendors.knownFolders);
-    exe_tests.setBuildMode(mode);
-    exe_tests.setTarget(target);
+    const exe_tests = b.addTest(.{ .root_source_file = .{ .path = "src/tests.zig" } });
+    exe_tests.addModule("known-folders", knownFolders);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
