a7baf68b960135a267c1e87260c7ba784a93733b
yuekcc <yuekcc@qq.com>
2023-02-08 00:15:07 +0800

    clean

--- build.zig
+++ build.zig
@@ -1,37 +1,28 @@
 const std = @import("std");
 
-const vendors = struct {
-    pub const knownFolders = std.build.Pkg{
-        .name = "known-folders",
-        .source = std.build.FileSource{
-            .path = "vendors/known-folders/known-folders.zig",
-        },
-    };
-};
-
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const knownFolders = b.createModule(.{
+    const known_folders_module = b.createModule(.{
         .source_file = .{ .path = "vendors/known-folders/known-folders.zig" },
     });
 
-    const exe = b.addExecutable(.{
+    const app = b.addExecutable(.{
         .name = "bash_prompt",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    exe.single_threaded = true;
-    exe.strip = optimize != .Debug;
-    exe.want_lto = optimize != .Debug;
-    exe.addModule("known-folders", knownFolders);
-    exe.install();
+    app.single_threaded = true;
+    app.strip = optimize != .Debug;
+    app.want_lto = optimize != .Debug;
+    app.addModule("known-folders", known_folders_module);
+    app.install();
 
-    const run_cmd = exe.run();
+    const run_cmd = app.run();
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -40,9 +31,9 @@ pub fn build(b: *std.Build) !void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest(.{ .root_source_file = .{ .path = "src/tests.zig" } });
-    exe_tests.addModule("known-folders", knownFolders);
+    const app_tests = b.addTest(.{ .root_source_file = .{ .path = "src/tests.zig" } });
+    app_tests.addModule("known-folders", known_folders_module);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    test_step.dependOn(&app_tests.step);
 }
