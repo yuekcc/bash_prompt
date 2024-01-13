1ba2103b21f47e2edd64217c73c9f70a9fc217b9
yuekcc <yuekcc@qq.com>
2024-01-13 23:14:33 +0800

    styles

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
     const known_folders_module = b.createModule(.{
@@ -17,23 +16,28 @@ pub fn build(b: *std.Build) !void {
         .strip = true,
         .single_threaded = true,
     });
-
     app.root_module.addImport("known-folders", known_folders_module);
 
     b.installArtifact(app);
 
-    const run_cmd = b.addRunArtifact(app);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+    // zig build run
+    {
+        const run_cmd = b.addRunArtifact(app);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // zig build test
+    {
+        const app_tests = b.addTest(.{ .root_source_file = .{ .path = "src/tests.zig" } });
+        app_tests.root_module.addImport("known-folders", known_folders_module);
 
-    const app_tests = b.addTest(.{ .root_source_file = .{ .path = "src/tests.zig" } });
-    app_tests.root_module.addImport("known-folders", known_folders_module);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&app_tests.step);
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&app_tests.step);
+    }
 }
